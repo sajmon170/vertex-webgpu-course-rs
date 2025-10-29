4099b73f256dfec543ef0e94c09208018f6e3ed4
sajmon <szymon17099@gmail.com>
2025-10-29 12:44:53 +0100

    Add render pass code

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ impl ApplicationHandler for App {
                     gpu.resize(size);
                 }
                 WindowEvent::RedrawRequested => {
-                    todo!();
+                    gpu.render().unwrap()
                 }
                 _ => (),
             }
@@ -122,6 +122,46 @@ impl Gpu {
     pub fn resize(&mut self, size: PhysicalSize<u32>) {
         self.config.width = size.width;
         self.config.height = size.height;
+    }
+
+    pub fn render(&self) -> Result<()> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    depth_slice: None,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.0,
+                            g: 0.0,
+                            b: 0.0,
+                            a: 0.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 }
 
