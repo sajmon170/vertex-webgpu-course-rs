2d620a2fd2df89d30c01dc9cd0334531ee4134f8
sajmon <szymon17099@gmail.com>
2025-10-29 11:56:33 +0100

    Add winit boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use winit::{
+    application::ApplicationHandler,
+    dpi::PhysicalSize,
+    event::WindowEvent,
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+#[derive(Default)]
+struct App;
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        let size = PhysicalSize::new(640, 480);
+
+        let attrs = Window::default_attributes()
+            .with_inner_size(size.clone())
+            .with_resizable(false);
+
+        let _window = event_loop.create_window(attrs).unwrap();
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("Closing window.");
+                event_loop.exit();
+            }
+            WindowEvent::Resized(_size) => {
+                todo!();
+            }
+            WindowEvent::RedrawRequested => {
+                todo!();
+            }
+            _ => (),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.run_app(&mut App::default()).unwrap();
 }
