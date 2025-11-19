eb3ba83fdd8823fe6da37f6753210bd0c71e1404
sajmon <szymon17099@gmail.com>
2025-11-19 15:13:08 +0100

    Add indexed drawing

--- src/main.rs
+++ src/main.rs
@@ -256,7 +256,9 @@ impl Gpu {
             });
 
             render_pass.set_pipeline(&self.pipeline);
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..Gpu::INDICES.len() as u32, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
